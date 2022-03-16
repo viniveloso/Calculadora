3313d6ccc8fc8ae02f97f08dec0f27e3b7fc07b6
Vinicius Veloso <viniciusveloso01@gmail.com>
2022-03-15 21:44:48 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        // Calculadora simples.
+
+        int numero1, numero2, resultado = 0;
+        string operacao;
+
+        Console.WriteLine("Digite o primeiro número: ");
+        numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a operação: ");
+        operacao = Console.ReadLine();
+
+        Console.WriteLine("Digite o segundo número: ");
+        numero2 = int.Parse(Console.ReadLine());
+
+        switch (operacao)
+        {
+                case "+":
+                resultado = numero1 + numero2;
+                break;
+
+                case "-":
+                resultado = numero1 - numero2;
+                break;
+
+                case "/":
+                resultado = numero1 / numero2;
+                break;
+
+                case "*":
+                resultado = numero1 * numero2;
+                break;
+
+                default:
+            Console.WriteLine("Operação invalida!");
+                break;
+        }
+
+        Console.WriteLine("O resultado é: ");
+
+        Console.WriteLine("{0} {1} {2} = {3}", numero1, operacao, numero2, resultado);
+
+        Console.ReadKey(true);
+    }
+}
